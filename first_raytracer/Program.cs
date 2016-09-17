aee0864f9c10d1581c8652b8bd12d4d9bbbf022f
wtyqm <wtyqm@126.com>
2016-09-17 22:20:18 +0800

    add source cpp

--- first_raytracer/Program.cs
+++ first_raytracer/Program.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ConsoleApplication
+namespace Practice3d
 {
-    public class Program
+    public class Vec3<Type>
     {
+            public Type x,y,z;
+    }
+
+}
+
+namespace Practice3d
+{
+    using Vec3f = Vec3<float>;
+
+    public class Sphere
+    {
+        public Vec3f origin;
+        public float radius;
+    }
+
+    public class FirstRayTracer
+    {
+        Vec3f trace(Vec3f rayOrigin, Vec3f rayDir, List<Sphere> objs, int depth)
+        {
+            //find the most close object hit by the ray
+            //get itself color
+            //get reflect color
+            //get refraction color
+            //mix
+            return new Vec3f();
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
